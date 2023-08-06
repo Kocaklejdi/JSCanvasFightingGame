48afc32b593c74171c2e65686e79101d0a975e01
unknown <kocaklejdi@gmail.com>
2023-08-06 20:18:49 +0200

    first commit

---
+++ src/classes.js
@@ -0,0 +1,156 @@
+
+
+class controls{
+    constructor(){
+        this.left = false;
+        this.right = false;
+        this.jump = false;
+        this.attack = false;
+        this.cooldown = 1000;
+        this.lastPressed = 0;
+    }
+    updateControls(){
+        document.onkeydown = (e) => {
+            if(e.key === "ArrowLeft"){
+                this.left = true;
+            }
+            if(e.key === "ArrowRight"){
+                this.right = true;
+            }
+            if(e.key === " "){
+                this.jump = true;
+            }
+            if(e.key === "z"){
+                if(this.lastPressed <= (Date.now()-this.cooldown)){
+                    this.attack = true;
+                    this.lastPressed = Date.now();
+                    setTimeout(()=>{this.attack = false},600)
+                }
+            }
+
+        }
+        document.onkeyup = (e) => {
+            if(e.key === "ArrowLeft"){
+                this.left = false;
+            }
+            if(e.key === "ArrowRight"){
+                this.right = false;
+            }
+            if(e.key === " "){
+                this.jump = false;
+            }
+        }
+    }
+}
+
+class sword{
+    constructor(x,y,width,height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+    swing(x,y,other){
+        this.x = other.x;
+        this.y = other.y;
+        // ctx.beginPath();
+        // ctx.rect(x,y,this.width,this.height);
+        // ctx.fillStyle = 'black';
+        // ctx.fill();
+        // ctx.stroke();
+    }
+}
+
+const idle = document.getElementById("idle");
+const running = document.getElementById("running");
+const attacking = document.getElementById("attacking");
+const jumping = document.getElementById("running");
+
+class player{
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.image = idle;
+        this.xSpeed = 5;
+        this.ySpeed = 3;
+        this.isFalling = false;
+        this.weapon = new sword(this.x,this.y,30,10);
+        this.controls = new controls();
+        this.frame = 0;
+        this.noOfFrames = 4;
+        this.width = 32;
+        this.height = 32;
+    }
+    drawSelf(){
+        if(!(this.controls.left || this.controls.right || this.controls.attack || this.controls.jump)){
+            this.noOfFrames = 4;
+            this.image = idle;
+        } else if(this.controls.left || this.controls.right){
+            this.noOfFrames = 6;
+            this.image = running;
+        }
+        if(this.controls.attack){
+            this.weapon.swing(this.x + 10,this.y + 10,this);
+            this.noOfFrames = 4;
+            this.image = attacking
+        } else {
+            this.weapon.x = -100;
+            this.weapon.y = -100;
+        }
+        ctx.drawImage(this.image,32*this.frame,0,32,32,this.x,this.y,this.width,this.height);
+    }
+    update(){
+        if(this.frame > this.noOfFrames){
+            this.frame = 0;
+        }
+        if(this.y != 100){
+            this.isFalling = true;
+        }else{
+            this.isFalling = false;
+        }
+        if(this.isFalling){
+            this.controls.jump = false;
+            this.ySpeed += 1;
+        }
+        if(this.controls.left){
+            this.x -= this.xSpeed;
+        }
+        if(this.controls.right){
+            this.x += this.xSpeed;
+        }
+        if(this.controls.jump){
+            this.ySpeed = -10;
+        }
+        if(this.y > 100){
+            this.ySpeed = 0;
+            this.y = 100; 
+        }
+        this.y += this.ySpeed
+    }
+}
+
+class enemy extends player{
+    constructor(x,y){
+        super(x,y);
+        this.hp = 100;
+        this.xSpeed = 4;  
+    }
+    ai(other){
+        if(this.x - 20 > other.x){
+            this.controls.left = true;
+            this.controls.right = false;
+        } else {
+            this.controls.left = false;
+        }
+        if(this.x < other.x){
+            this.controls.right = true;
+            this.controls.left = false;
+        }
+        if(other.controls.attack){
+            if(Math.random() < 0.5){
+                this.controls.right = true;
+                this.controls.left = false;
+            }
+        }
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,59 @@
+const body = document.getElementById("myBody");
+const ctx = document.getElementById("canvas").getContext("2d");
+
+const image1 = document.getElementById("s1")
+const image2 = document.getElementById("s2")
+const image3 = document.getElementById("s3")
+const image4 = document.getElementById("s4")
+
+
+
+function drawBackground(){
+    ctx.drawImage(image1,0,0)
+    ctx.drawImage(image2,0,0)
+    ctx.drawImage(image3,0,0)
+    ctx.drawImage(image4,-100,-60) 
+}
+
+function checkPlayerBounds(obj){
+  if(obj.x < 0){
+    obj.x = 0;
+  }
+  if(obj.x > 275){
+    obj.x = 275;
+  }
+}
+
+function collision(player1,player2){
+  if(player1.x < player2.x + player2.width &&
+    player1.x + player1.width > player2.x &&
+    player1.y < player2.y + player2.height &&
+    player1.y + player1.height > player2.y)
+{
+    player2.hp --;
+}
+}
+
+
+
+const player1 = new player(100,60);
+const enemy1 = new enemy(200,60);
+const hp = document.getElementById("hp");
+
+
+function drawGame(){
+  hp.style.width = `${(enemy1.hp/100) * 500}px`;
+  drawBackground();
+  checkPlayerBounds(player1);
+  player1.drawSelf();
+  player1.update();
+  player1.controls.updateControls();
+  enemy1.drawSelf();
+  enemy1.update();
+  enemy1.ai(player1);
+  collision(player1.weapon,enemy1)
+}
+
+setInterval(()=>{player1.frame += 1},100)
+
+setInterval(drawGame,1000/25)
